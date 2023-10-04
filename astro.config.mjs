bb145fcb1a970d0864bc7b11a51d3dc50a387f91
jeshol <jesholmlund@gmail.com>
2023-10-04 23:35:35 +0200

    hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
+  output: "hybrid",
   adapter: netlify(),
 });
