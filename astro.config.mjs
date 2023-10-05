e2749e63c1d8100c465088a7ef22d9928793ab82
jeshol <jesholmlund@gmail.com>
2023-10-05 10:08:38 +0200

    cleaning

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify/functions";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
+  output: "hybrid",
   adapter: netlify(),
 });
